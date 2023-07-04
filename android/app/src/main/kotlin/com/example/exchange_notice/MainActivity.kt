e4704d55d4226de1c1bfb0fb2520dda026f58629
Malik ATIF Ali <Malikatifali94@gmail.com>
2023-07-04 15:46:43 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/exchange_notice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exchange_notice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/component/padding.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+const Toppadding =EdgeInsets.only(left: 10,right: 10,top: 20,bottom: 0);
